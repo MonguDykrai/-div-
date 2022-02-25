13c49ed88284a56684de5aed2578814d8b7f6c70
MonguDykrai <kisaihou@gmail.com>
2022-02-25 15:32:27 +0800

    获取 point1, point2 的坐标调用 addLine 方法 draw line

--- addLine.js
+++ addLine.js
@@ -9,8 +9,8 @@ function addLine(id, x1, y1, x2, y2) {
   // Math.atan2() 返回从原点(0,0)到(x,y)点的线段与x轴正方向之间的平面角度(弧度值)，也就是Math.atan2(y,x)
   // Math.atan2((y2 - y1), (x2 - x1)) 求弧度
   // 弧度 * (180 / Math.PI) 求角度
-  const ang = Math.atan2(y2 - y1, x2 - x1) * (180 / Math.PI);
-  // const ang = (Math.atan2(y2 - y1, x2 - x1) * 180) / Math.PI;
+  // const ang = Math.atan2(y2 - y1, x2 - x1) * (180 / Math.PI);
+  const ang = (Math.atan2(y2 - y1, x2 - x1) * 180) / Math.PI;
   newLine.style.top = `${x1}px`;
   newLine.style.left = `${y1}px`;
   newLine.style.width = `${length}px`;
@@ -23,6 +23,6 @@ function addLine(id, x1, y1, x2, y2) {
   canvas.appendChild(newLine);
 }
 
-addLine('container', 50, 40, 100, 180);
-addLine('container', 80, 50, 50, 150);
-addLine('container', 50, 50, 150, 50);
+// addLine('container', 50, 40, 100, 180);
+// addLine('container', 80, 50, 50, 150);
+// addLine('container', 50, 50, 150, 50);
